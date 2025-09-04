07b0e97797622733abadf1f894fe7f1011b5656f
kosadmin <info@koutsourcing.vn>
2025-09-04 09:11:12 +0700

    update code 2

---
+++ js/hamburger-btn.js
@@ -0,0 +1,6 @@
+let menuList = document.getElementById("menu-btn");
+
+function menuFunction(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
+}
